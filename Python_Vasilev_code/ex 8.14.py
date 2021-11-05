ddaeca6a759adeb9cfbde7b81c3778e5bd679201
Artem1312 <48861960+Artem1312@users.noreply.github.com>
2021-11-05 19:20:21 +0200

    Delete Python_Vasilev_code directory

--- Python_Vasilev_code/ex 8.14.py
+++
@@ -1,9 +0,0 @@
-s = iter([1,2,3])
-print(next(s))
-print(next(s))
-print(next(s))
-
-try:
-    print(next(s))
-except Exception as err:
-    print(err.__class__)
--- Python_Vasilev_code/ex 8.15.py
+++
@@ -1,9 +0,0 @@
-s = [1,2,3].__iter__()
-print(s.__next__())
-print(s.__next__())
-print(s.__next__())
-
-try:
-    print(s.__next__())
-except Exception as err:
-    print(err.__class__)
